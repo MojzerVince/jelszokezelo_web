3df53f630ab2024d3766c5eedd2f210a5a29e5ef
Mojzi PC <Mojzi@DESKTOP-CG1M4VP>
2024-03-31 17:19:12 +0200

    code for making links non-draggable

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function Add(item){
     alert("NIGA!");
 }
+
+let text = document.getElementsByTagName("a");
+for (let i = 0; i < text.length; i++){
+    text[i].setAttribute("draggable", false);
+    console.log(text[i]);
+}
